118c6354cff085395510e99b7ff8a56a3004eed4
Luiz Picanço <lpicanco@gmail.com>
2023-02-20 01:23:37 +0000

    publish to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.8.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     id("org.sonarqube") version "3.4.0.2513"
     jacoco
+    id("maven-publish")
+    id("signing")
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
 allprojects {
     group = "com.neutrine.krate"
-    version = "1.0-SNAPSHOT"
+    version = "1.0"
 
     repositories {
         mavenCentral()
@@ -26,10 +29,14 @@ sonarqube {
     }
 }
 
+val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
+
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
     apply(plugin = "jacoco")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     dependencies {
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
@@ -52,7 +59,67 @@ subprojects {
     }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
         kotlinOptions.freeCompilerArgs += "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "io.github.lpicanco"
+                artifactId = project.name
+                version = project.version.toString()
+
+                from(components["java"])
+
+                pom {
+                    name.set("$groupId:${project.name}")
+                    description.set("Rate Limit Library for Kotlin.")
+                    url.set("https://github.com/lpicanco/krate")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            distribution.set("repo")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("lpicanco")
+                            name.set("Luiz Picanço")
+                            email.set("lpicanco@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/lpicanco/krate.git")
+                        developerConnection.set("scm:git:https://github.com/lpicanco/krate.git")
+                        url.set("https://github.com/lpicanco/krate")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                val releaseRepo = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                val snapshotRepo = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+
+                name = "OSSRH"
+                url = if (isReleaseVersion) releaseRepo else snapshotRepo
+
+                credentials {
+                    username = findProperty("ossrhUsername")?.toString() ?: System.getenv("OSSRH_USERNAME")
+                    password = findProperty("ossrhPassword")?.toString() ?: System.getenv("OSSRH_PASSWORD")
+                }
+            }
+        }
+    }
+
+    configure<SigningExtension> {
+        sign(project.extensions.getByType<PublishingExtension>().publications["maven"])
+    }
+
+    tasks.withType<Sign> {
+        onlyIf { isReleaseVersion }
     }
 }
